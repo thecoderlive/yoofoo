fa9f6899a5c211ab9be38e67616d7228d58070c4
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-19 05:14:43 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "f1661f72-0b98-4bba-aa43-6eec52164921",
+    "home_feed": [
+        {
+            "id": "427d8419-ea8b-4534-a7c4-6fe8c2f240f2",
+            "artist_name": "analysis",
+            "home_carousel": [
+                {
+                    "id": "89e71ea7-dc32-4058-864e-a02d37b9624a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+                },
+                {
+                    "id": "7e2f1372-2f19-4744-bf4c-713ecf355eb0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/233.png"
+                },
+                {
+                    "id": "d8acc36f-5de7-499a-9115-9d8ce80ee810",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/287.png"
+                },
+                {
+                    "id": "84edfedb-232c-42c5-a744-d5bc40ba1a2d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                },
+                {
+                    "id": "eae426f1-9c3b-404c-b594-6607f1f1eb95",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png"
+                }
+            ],
+            "art_caption": "beyond",
+            "art_description": "Different deal gun drug. Region manage great season several development."
+        },
+        {
+            "id": "661e3361-e8e7-4295-aaac-e8126f1f887f",
+            "artist_name": "religious",
+            "home_carousel": [
+                {
+                    "id": "88368ab7-5d82-442e-84c5-897ddeb55e54",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png"
+                },
+                {
+                    "id": "75de8d5f-b02d-4bec-b44b-5f2ce6821e0b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                },
+                {
+                    "id": "61205953-a80d-4986-9a3e-4a7abfcbd9a8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/238.png"
+                },
+                {
+                    "id": "e35040b2-bce8-4043-98bd-14cfc8b47aeb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/246.png"
+                },
+                {
+                    "id": "da85b6a3-cb71-4da7-9051-a5a775dd10b5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                }
+            ],
+            "art_caption": "expert",
+            "art_description": "Subject those bank so right. None clearly focus. Research PM age laugh wrong word ball."
+        },
+        {
+            "id": "25480b88-1077-43de-b5de-f83567dbe0b4",
+            "artist_name": "general",
+            "home_carousel": [
+                {
+                    "id": "40c25bba-ffa8-47dd-89e1-10dddf84991b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                },
+                {
+                    "id": "d0d2231c-1baf-4d4c-bf64-2d2ad782981d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/233.png"
+                },
+                {
+                    "id": "7349ae86-f22e-433d-89e9-dc9dc408d077",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/195.png"
+                },
+                {
+                    "id": "78d83cd6-c9c1-4f10-9b4a-1ee1aeeed864",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/50.png"
+                },
+                {
+                    "id": "3ba73605-37bb-48db-a95a-4e4b4a5b5d63",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/169.png"
+                }
+            ],
+            "art_caption": "specific",
+            "art_description": "Anyone community third service technology. Hotel most buy college ever remain service physical. Attack cover myself front plan walk serve."
+        },
+        {
+            "id": "8404c80f-3e74-44ae-86c7-2850d1a95e55",
+            "artist_name": "read",
+            "home_carousel": [
+                {
+                    "id": "482187c5-1b91-4fc4-b3f6-33b410d8084a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/320.png"
+                },
+                {
+                    "id": "26b64061-efcd-4b0f-bb98-a0ab51674f9f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/151.png"
+                },
+                {
+                    "id": "e5bf42a2-fd0c-4d85-9208-cd91edda0dc5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/61.png"
+                },
+                {
+                    "id": "6d390fd3-998d-48e8-a1d6-5b7a06b8e8cb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/294.png"
+                },
+                {
+                    "id": "d699fe9d-8242-42cc-ad84-f2548c112ed6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/133.png"
+                }
+            ],
+            "art_caption": "it",
+            "art_description": "Sister old all no interesting. Professional whatever particularly sometimes already."
+        },
+        {
+            "id": "1f6ebd49-9927-4866-8af4-696350a83fe1",
+            "artist_name": "trade",
+            "home_carousel": [
+                {
+                    "id": "39141ccf-5289-4086-a2af-5d47ccc0ca54",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                },
+                {
+                    "id": "427358af-f5fa-4fdc-8ecc-3c59bb874b6f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "8540c7a0-ab9c-4eee-b8ce-bdc7418aec56",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                },
+                {
+                    "id": "d8ba062c-0760-42ca-9b95-264aa4b2372b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+                },
+                {
+                    "id": "9b829bd7-a02b-4f8c-b772-340a0e34d69b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/239.png"
+                }
+            ],
+            "art_caption": "special",
+            "art_description": "Those situation right care push yourself somebody. Would spend draw item ahead side win. Season level you up that analysis her officer. Occur economy news Mrs sound happy risk."
+        }
+    ]
+}
